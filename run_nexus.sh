31e818db7da54b07cfc89e15ecaba68416a10b1b
Kali <229161133+akdmjeau-eng@users.noreply.github.com>
2026-04-25 03:03:15 +0000

    Crear run_nexus.sh

---
+++ run_nexus.sh
@@ -0,0 +1,66 @@
+
+#!/bin/bash
+# 
+#
+RUTA1="/root/path/to/venv/bin/activate"
+RUTA2="/root/Nexus/venv/bin/activate"
+
+if [ -f "$RUTA1" ]; then
+    source "$RUTA1"
+    echo "[] Entorno activado desde: path/to/venv"
+elif [ -f "$RUTA2" ]; then
+    source "$RUTA2"
+    echo "[] Entorno activado desde: Nexus/venv"
+else
+    #
+    EXTRA=$(find /root -name "activate" -path "*/bin/*" | head -n 1)
+    if [ -n "$EXTRA" ]; then
+        source "$EXTRA"
+        echo "[✔] Entorno detectado en ruta alterna: $EXTRA"
+    else
+        echo "[!] ERROR: No se encontró el venv. Ejecuta: python3 -m venv /root/Nexus/venv"
+        exit 1
+    fi
+fi
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+#
+if [ -f "/root/GhostTrack/GhostTR.py" ]; then
+    echo "[*] Iniciando GhostTrack..."
+    python3 /root/GhostTrack/GhostTR.py
+else
+    echo "[!] Error: GhostTR.py no está en /root/GhostTrack/"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                          [ Read 34 lines ]
+^G Help       ^O Write Out  ^F Where Is   ^K Cut        ^T Execute    ^C Location   M-U Undo
+^X Exit       ^R Read File  ^\ Replace    ^U Paste      ^J Justify    ^/ Go To Line M-E Redo
